c578d2a821775268b9ecaa2e2e4b090b2a389118
Randima Dias <randimamethminid525@gmail.com>
2024-07-19 14:22:52 +0530

    feat: create Home, About, Header, Footer components

---
+++ yogoReactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-800">Hello</h1>
+      <h2 className="font-bold">React router dom</h2>
+    </>
+  );
+}
+
+export default App;
